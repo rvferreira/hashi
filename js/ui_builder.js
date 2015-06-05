0eaed3275dc1ace10bf735f5fb134325d7f7a800
Raphael Ferreira <raphael.rvf@gmail.com>
2015-06-04 21:08:15 -0300

    skeleton

--- js/ui_builder.js
+++ js/ui_builder.js
@@ -1,4 +1,47 @@
-HEADER_HEIGHT = 0.20;
+HEADER_HEIGHT = 0.08;
+
+var pageWidth;
+var pageHeight;
+var headerHeight;
 
 function layoutInit() {
+	pageWidth = window.innerWidth;
+	pageHeight = window.innerHeight;
+	
+	$("#fullpage").css({
+		height: function(){
+			return pageHeight;
+		},
+		width: function(){
+			return pageWidth;
+		}
+	});
+	
+	$("#header").css({
+		height: function(){
+			headerHeight = $("#fullpage").height() * HEADER_HEIGHT;
+			return headerHeight;
+		},
+		width: function(){
+			return $("#fullpage").width();
+		}
+	});
+	
+	$("#logo").css({
+		height: function(){
+			return headerHeight;
+		},
+		width: function(){
+			return headerHeight * (5/2);
+		}
+	});
+	
+	$("#menuItems").css({
+		left: function(){
+			return (pageWidth/2 - $(this).width()/2);
+		},
+		fontSize: function(){
+			return headerHeight * 0.5;
+		}
+	});
 }
