9bc6d45dbdcab84a121d354267a6cf99b4f9e35e
darambaris <jessika.darambaris@gmail.com>
2015-06-12 14:33:28 -0300

    mesa, filósofos e menu

--- js/ui_builder.js
+++ js/ui_builder.js
@@ -3,8 +3,10 @@ HEADER_HEIGHT = 0.08;
 var pageWidth;
 var pageHeight;
 var headerHeight;
+var table;
 
-function layoutInit() {
+jQuery(document).ready(function($){
+	
 	pageWidth = window.innerWidth;
 	pageHeight = window.innerHeight;
 	
@@ -20,6 +22,7 @@ function layoutInit() {
 	$("#header").css({
 		height: function(){
 			headerHeight = $("#fullpage").height() * HEADER_HEIGHT;
+
 			return headerHeight;
 		},
 		width: function(){
@@ -30,18 +33,74 @@ function layoutInit() {
 	$("#logo").css({
 		height: function(){
 			return headerHeight;
-		},
-		width: function(){
-			return headerHeight * (5/2);
+		}
+	});
+
+	$(".lang_img").css({
+		height: function(){
+			return headerHeight * (8/10);
+			
 		}
 	});
 	
+	
 	$("#menuItems").css({
-		left: function(){
-			return (pageWidth/2 - $(this).width()/2);
-		},
+		
 		fontSize: function(){
 			return headerHeight * 0.5;
+		},
+
+		left: function(){
+			return (pageWidth-$("#menuItems").width())/2;
 		}
 	});
-}
+
+	$("#table").css({
+		width: function(){
+			return (table = pageWidth*(3/10));
+		},
+		left: function(){
+			return (pageWidth-$(this).width())/2;
+		},
+		top: function(){
+			/* metade do complemento do valor escolhido para height */
+			return pageHeight*(1.1/10);
+		}
+	});
+
+	$(".philosopher").css({
+		height: function(){
+			return(table*0.3);
+		},
+		left: function(){	
+				return (pageWidth*0.5 -$(this).width()/2);
+		},
+		top: function(){	
+				return (table*1.28 -$(this).width()/2);
+		}
+	});
+		$(".left").css({
+			left: function(){	
+				return ((pageWidth/2-table/2)*0.94-$(this).width()/2);
+			}
+
+		});
+
+		$(".right").css({
+			left: function(){	
+				return (pageWidth/2+table/2)*0.98;
+			}
+		});
+		$(".top").css({
+			top: function(){	
+				return (table*0.25 -$(this).width()/2);
+			}
+		});
+		
+		$(".bottom").css({
+			top: function(){	
+				return (table*0.95 -$(this).width()/2);
+			}
+		});
+
+});
