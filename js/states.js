c825658f1319c00ec9889cafb3c5ace9378cada4
Raphael Ferreira <raphael.rvf@gmail.com>
2015-06-14 11:07:00 -0300

    state files

---
+++ js/states.js
@@ -0,0 +1,9 @@
+function next() {
+    state.frame += 1;
+    console.log("Indo para o frame :"+state.frame);
+}
+
+function prev() {
+    state.frame -= 1;
+    console.log("Indo para o frame :"+state.frame);
+}
